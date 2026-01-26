3da58f2fbee9cc3849247f3f051e24de06ba818f
Divyansh Agarwal <divyanshagarwal188@gmail.com>
2026-01-26 14:16:39 -0500

    update bronze proc added

--- scripts/exec.sql
+++ scripts/exec.sql
@@ -10,6 +10,8 @@ EXECUTE DataWarehouse.bronze.load_bronze;
 -- Note: the update script is idempotent and can be run multiple times without adverse effects
 -- scripts/bronze/update_bronze.sql
 
+-- update:  now we have a procedure for it called update_bronze
+
 EXECUTE DataWarehouse.bronze.update_bronze;
 
 EXECUTE DataWarehouse.silver.load_silver;
